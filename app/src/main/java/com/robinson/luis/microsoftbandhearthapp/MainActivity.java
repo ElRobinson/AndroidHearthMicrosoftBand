656cd70572ce90f3eaa87a7e5aadff19fffd7c89
ElRobinson <luisfelipe.robinson@gmail.com>
2016-09-14 16:15:36 -0300

    Updates

--- app/src/main/java/com/robinson/luis/microsoftbandhearthapp/MainActivity.java
+++ app/src/main/java/com/robinson/luis/microsoftbandhearthapp/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.lang.ref.WeakReference;
-
 import com.microsoft.band.BandClient;
 import com.microsoft.band.BandClientManager;
 import com.microsoft.band.BandException;
@@ -18,26 +16,21 @@ import com.microsoft.band.UserConsent;
 
 import com.microsoft.band.sensors.BandHeartRateEvent;
 import com.microsoft.band.sensors.BandHeartRateEventListener;
-import com.microsoft.band.sensors.HeartRateConsentListener;
-import android.os.Bundle;
-import android.view.View;
-import android.app.Activity;
+
 import android.os.AsyncTask;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private BandClient client = null;
-    private Button btnStart, btnConsent;
+    private Button btnStart;
     private TextView txtStatus;
 
     private BandHeartRateEventListener mHeartRateEventListener = new BandHeartRateEventListener() {
         @Override
         public void onBandHeartRateChanged(final BandHeartRateEvent event) {
             if (event != null) {
-                appendToUI(String.format("Batimentos  = %d batimentos por minutos\n"
+                appendToUI(String.format("Batimentos  = %d batimentos por minuto\n"
                         + "Status = %s\n", event.getHeartRate(), event.getQuality()));
             }
         }
@@ -50,22 +43,12 @@ public class MainActivity extends AppCompatActivity {
 
         txtStatus = (TextView) findViewById(R.id.txtStatus);
         btnStart = (Button) findViewById(R.id.btnStart);
+
         btnStart.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 txtStatus.setText("");
                 new HeartRateSubscriptionTask().execute();
-            }
-        });
-
-        final WeakReference<Activity> reference = new WeakReference<Activity>(this);
-
-        btnConsent = (Button) findViewById(R.id.btnConsent);
-        btnConsent.setOnClickListener(new OnClickListener() {
-            @SuppressWarnings("unchecked")
-            @Override
-            public void onClick(View v) {
-                new HeartRateConsentTask().execute(reference);
             }
         });
     }
@@ -94,9 +77,9 @@ public class MainActivity extends AppCompatActivity {
             try {
                 client.disconnect().await();
             } catch (InterruptedException e) {
-                // Do nothing as this is happening during destroy
+
             } catch (BandException e) {
-                // Do nothing as this is happening during destroy
+
             }
         }
         super.onDestroy();
@@ -122,7 +105,6 @@ public class MainActivity extends AppCompatActivity {
         } else if (ConnectionState.CONNECTED == client.getConnectionState()) {
             return true;
         }
-
         appendToUI("Conectando\n");
         return ConnectionState.CONNECTED == client.connect().await();
     }
@@ -141,19 +123,7 @@ public class MainActivity extends AppCompatActivity {
                     appendToUI("Band não está conectada no bluetooth.\n");
                 }
             } catch (BandException e) {
-                String exceptionMessage="";
-                switch (e.getErrorType()) {
-                    case UNSUPPORTED_SDK_VERSION_ERROR:
-                        exceptionMessage = "SDK muito baixo\n";
-                        break;
-                    case SERVICE_ERROR:
-                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
-                        break;
-                    default:
-                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
-                        break;
-                }
-                appendToUI(exceptionMessage);
+                appendToUI("Erro:" + e.getMessage());
 
             } catch (Exception e) {
                 appendToUI(e.getMessage());
@@ -162,42 +132,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class HeartRateConsentTask extends AsyncTask<WeakReference<Activity>, Void, Void> {
-        @Override
-        protected Void doInBackground(WeakReference<Activity>... params) {
-            try {
-                if (getConnectedBandClient()) {
-
-                    if (params[0].get() != null) {
-                        client.getSensorManager().requestHeartRateConsent(params[0].get(), new HeartRateConsentListener() {
-                            @Override
-                            public void userAccepted(boolean consentGiven) {
-                            }
-                        });
-                    }
-                } else {
-                    appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
-                }
-            } catch (BandException e) {
-                String exceptionMessage="";
-                switch (e.getErrorType()) {
-                    case UNSUPPORTED_SDK_VERSION_ERROR:
-                        exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
-                        break;
-                    case SERVICE_ERROR:
-                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
-                        break;
-                    default:
-                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
-                        break;
-                }
-                appendToUI(exceptionMessage);
-
-            } catch (Exception e) {
-                appendToUI(e.getMessage());
-            }
-            return null;
-        }
-    }
 }
 
